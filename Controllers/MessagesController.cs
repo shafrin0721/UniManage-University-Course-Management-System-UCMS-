629e7b41a990a2be9c5974e8be1436ba5571fda4
Praneeth Jayesha <pathiranajayesh@gmail.com>
2026-03-24 18:02:11 +0530

    Added Lecturer Dashboard improvements, Assignment Edit/Delete/Submissions, Communication Module enhancements

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using UniManage.DAL;
@@ -15,16 +16,56 @@ namespace UniManage.Controllers
         public ActionResult Inbox()
         {
             int userId = (int)Session["UserId"];
-            var data = db.Messages.Where(m => m.ToUserId == userId).OrderByDescending(m => m.SentAt).ToList();
+            var data = db.Messages
+                .Where(m => m.ToUserId == userId)
+                .OrderByDescending(m => m.SentAt)
+                .ToList();
+
+            ViewBag.UserNames = db.AppUsers
+                .ToDictionary(u => u.Id, u => u.FullName);
+
             return View(data);
         }
 
-        [HttpGet]
-        public ActionResult Compose()
+        public ActionResult Sent()
         {
             int userId = (int)Session["UserId"];
-            ViewBag.ToUserId = new SelectList(db.AppUsers.Where(u => u.Id != userId), "Id", "FullName");
-            return View();
+            var data = db.Messages
+                .Where(m => m.FromUserId == userId)
+                .OrderByDescending(m => m.SentAt)
+                .ToList();
+
+            ViewBag.UserNames = db.AppUsers
+                .ToDictionary(u => u.Id, u => u.FullName);
+
+            return View(data);
+        }
+
+        public ActionResult MarkAsRead(int id)
+        {
+            var message = db.Messages.Find(id);
+            if (message != null)
+            {
+                message.IsRead = true;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Inbox");
+        }
+
+        [HttpGet]
+        public ActionResult Compose(int? replyToUserId = null, string replySubject = null)
+        {
+            int userId = (int)Session["UserId"];
+            ViewBag.ToUserId = new SelectList(
+                db.AppUsers.Where(u => u.Id != userId), "Id", "FullName");
+
+            if (replyToUserId.HasValue)
+            {
+                ViewBag.ReplyToUserId = replyToUserId.Value;
+                ViewBag.ReplySubject = replySubject;
+            }
+
+            return View(new Message());
         }
 
         [HttpPost]
@@ -38,12 +79,16 @@ namespace UniManage.Controllers
             if (!ModelState.IsValid)
             {
                 int userId = (int)Session["UserId"];
-                ViewBag.ToUserId = new SelectList(db.AppUsers.Where(u => u.Id != userId), "Id", "FullName", model.ToUserId);
+                ViewBag.ToUserId = new SelectList(
+                    db.AppUsers.Where(u => u.Id != userId),
+                    "Id", "FullName", model.ToUserId);
                 return View(model);
             }
 
             db.Messages.Add(model);
             db.SaveChanges();
+
+            TempData["Success"] = "Message sent successfully.";
             return RedirectToAction("Inbox");
         }
     }
